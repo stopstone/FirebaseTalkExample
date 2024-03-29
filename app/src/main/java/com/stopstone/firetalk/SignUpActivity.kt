3efb4552fe59e4fa8eb623db4e481402524ee19c
StopStone <wltjr6432@naver.com>
2024-03-30 01:13:16 +0900

    feat: 회원가입 입력창 유효성 검사 추가

--- app/src/main/java/com/stopstone/firetalk/SignUpActivity.kt
+++ app/src/main/java/com/stopstone/firetalk/SignUpActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
@@ -23,26 +24,37 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
+        auth = Firebase.auth
         setContentView(binding.root)
 
-        auth = Firebase.auth
+        setData()
         setListener()
     }
 
-    private fun setListener() {
-        binding.btnSignUp.setOnClickListener {
-            signUp()
+    private fun setData() {
+        with(binding) {
+            etSignUpNickname.addTextChangedListener {
+                name = it.toString()
+                updateButton()
+            }
+            etSignUpEmail.addTextChangedListener {
+                email = it.toString()
+                updateButton()
+            }
+            etSignUpPassword.addTextChangedListener {
+                password = it.toString()
+                updateButton()
+            }
         }
+    }
+
+    private fun updateButton() {
+        binding.btnSignUp.isEnabled = name.isNotBlank() && email.isNotBlank() && password.isNotBlank()
     }
 
     private fun signUp() {
         database = FirebaseDatabase.getInstance()
         val userRef = database.getReference("User")
-        with(binding) {
-            name = etSignUpNickname.text.toString()
-            email = etSignUpEmail.text.toString()
-            password = etSignUpPassword.text.toString()
-        }
 
         Firebase.auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
@@ -66,5 +78,11 @@ class SignUpActivity : AppCompatActivity() {
                 } else
                     Toast.makeText(this, "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show()
             }
+    }
+
+    private fun setListener() {
+        binding.btnSignUp.setOnClickListener {
+            signUp()
+        }
     }
 }
